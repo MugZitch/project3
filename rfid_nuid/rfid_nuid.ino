f74af232189894c16917027696448591fa4cc1c6
MugZitch <43876613+MugZitch@users.noreply.github.com>
2019-05-14 13:02:32 +0200

    Add files via upload

--- rfid_nuid/rfid_nuid.ino
+++ rfid_nuid/rfid_nuid.ino
@@ -1,19 +1,27 @@
-
-
-/*
- * 
- * All the resources for this project: http://randomnerdtutorials.com/
- * Modified by Rui Santos
- * 
- * Created by FILIPEFLOP
- * 
- */
- 
 #include <SPI.h>
 #include <MFRC522.h>
- 
+
+#include <Keypad.h>
+
+const byte ROWS = 4; // Four rows
+const byte COLS = 4; // Three columns
+// Define the Keymap
+char keys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+// Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
+byte rowPins[ROWS] = { 9, 8, 7, 6 };
+// Connect keypad COL0, COL1 and COL2 to these Arduino pins.
+byte colPins[COLS] = { 5, 4, 3, 2 }; 
+
+// Create the Keypad
+Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
 #define SS_PIN 10
-#define RST_PIN 9
+#define RST_PIN A0
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
  
 void setup() 
@@ -24,6 +32,11 @@ void setup()
 }
 void loop() 
 {
+   char key = kpd.getKey();
+  if(key)  // Check for a valid key.
+  {  
+    Serial.println(key);
+  }
   // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) 
   {
@@ -39,9 +52,9 @@ void loop()
   byte letter;
   for (byte i = 0; i < mfrc522.uid.size; i++) 
   {
-     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? "0" :"");
      Serial.print(mfrc522.uid.uidByte[i], HEX);
-     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : ""));
      content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
   Serial.println();
